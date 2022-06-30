c0a0e9465103725079f545c41b3d6b4e776c8f57
Jacob <jacobmunden@gmail.com>
2022-06-29 18:59:27 -0600

    commit tree 5142c440a8a31a1d01df8acb8d4ba4575a4d2328

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 62
+const num2 = 1
+const multiply = num1*num2;
+
+const random = Math.floor(Math.random() + 1);
+
+const num3 = 44
+const num4 = 10
+
+const mod = num3 % num4;
+
+const max = Math.max(0,5,10,20);
